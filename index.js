41c7326b41272960c8657882a2aff0eca223894b
stepan-mateiko <stepan-mateiko@gmail.com>
2023-12-21 13:46:53 +0200

    added hometask 2

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
-const getRandomNumber = () => {
-  return Math.random();
+const { exec } = require("child_process");
+const fs = require("fs");
+const os = require("os");
+
+const logFilePath = "activityMonitor.log";
+
+const getProcessInfoCommand = () => {
+  if (os.platform() === "win32") {
+    return "powershell \"Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + ' ' + $_.CPU + ' ' + $_.WorkingSet }\"";
+  } else {
+    return 'ps -e -o %cpu,%mem,comm --sort=-%cpu | head -n 2 | awk \'{print $1 " " $2 " " $3}\'';
+  }
 };
 
-console.log("Random number: ", getRandomNumber());
+const updateTopProcess = () => {
+  exec(getProcessInfoCommand(), (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error executing command: ${err}`);
+      return;
+    }
+
+    const processInfo = stdout.trim();
+    process.stdout.write(`\r${processInfo}`);
+
+    const timestamp = Math.floor(Date.now() / 1000);
+    const logMessage = `${timestamp} : ${processInfo}\n`;
+
+    fs.appendFile(logFilePath, logMessage, (err) => {
+      if (err) {
+        console.error(`Error writing to log file: ${err}`);
+      }
+    });
+  });
+};
+
+setInterval(updateTopProcess, 100);
+
+setInterval(() => {
+  const timestamp = Math.floor(Date.now() / 1000);
+  const logMessage = `${timestamp} : Log snapshot\n`;
+
+  fs.appendFile(logFilePath, logMessage, (err) => {
+    if (err) {
+      console.error(`Error writing to log file: ${err}`);
+    }
+  });
+}, 60000);
